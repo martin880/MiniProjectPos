b1584594a17fbe054890ef05155a16b326d26b17
Martin Halomoan Lumbangaol <martinlumbangaol880@gmail.com>
2023-05-30 15:21:16 +0700

    ok

---
+++ server/src/models/user.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, Sequelize) => {
+	const User = sequelize.define("Users", {
+		firstName: Sequelize.STRING,
+		lastName: Sequelize.STRING,
+		role: Sequelize.ENUM("Admin", "Kasir"),
+		email: Sequelize.STRING,
+		avatar: Sequelize.BLOB("long"),
+		phonenumber: Sequelize.STRING,
+		sex: Sequelize.STRING,
+		address: Sequelize.STRING,
+		ktp: Sequelize.STRING,
+		password: Sequelize.STRING,
+	});
+	return User;
+};
+z;
