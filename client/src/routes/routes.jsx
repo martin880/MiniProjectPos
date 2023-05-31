282694260ed592e926105007969638a1ab735d36
ThomasAlvin <thomasalvinyeo@gmail.com>
2023-05-31 11:50:16 +0700

    Searchbar dan Login fe

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,9 +1,11 @@
 import { Route } from "react-router-dom";
 import SearchBar from "../components/searchbar";
 import Cashier from "../pages/Cashier";
+import LoginPage from "../pages/loginpage";
 const routes = [
-	<Route path="/" element={<SearchBar />} />,
-	<Route path="/cashier" element={<Cashier />} />,
+  <Route path="/" element={<SearchBar />} />,
+  <Route path="/cashier" element={<Cashier />} />,
+  <Route path="/login" element={<LoginPage />} />,
 ];
 
 export default routes;
