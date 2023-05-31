0b94f8f9e829ae0a85f2797d2618fc4e91e2f087
Martin Halomoan Lumbangaol <martinlumbangaol880@gmail.com>
2023-05-31 10:47:07 +0700

    ok

--- client/src/pages/AdminPages.jsx
+++ client/src/pages/AdminPages.jsx
@@ -1,4 +1,12 @@
-import { Flex } from "@chakra-ui/react";
+import {
+	Flex,
+	Text,
+	Stack,
+	Input,
+	InputGroup,
+	InputLeftElement,
+	Icon,
+} from "@chakra-ui/react";
 import SideBar from "../components/SideBar";
 import TopBar from "../components/TopBar";
 
@@ -14,9 +22,28 @@ export default function Cashier() {
 						<Flex className="topbar">
 							<TopBar />
 						</Flex>
-						<Flex className="content">
-							{/* isi content */}
-							{/* isi content */}
+						<Flex className="adminCategory">
+							<Stack spacing={4}>
+								<Text
+									px={"4"}
+									fontSize={"24px"}
+									fontWeight={"bold"}
+									color={"black"}
+								>
+									Admin
+								</Text>
+								<InputGroup>
+									<InputLeftElement pointerEvents="none">
+										<Icon color="gray.300" />
+									</InputLeftElement>
+									<Input
+										type="text"
+										placeholder="Search Admin"
+										minW={"20vw"}
+										borderColor={"blackAlpha.300"}
+									/>
+								</InputGroup>
+							</Stack>
 						</Flex>
 					</Flex>
 				</Flex>
