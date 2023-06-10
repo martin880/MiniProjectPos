66a6dbf237572538abc83737b40aa5de12da7aaa
Martin Halomoan Lumbangaol <martinlumbangaol880@gmail.com>
2023-06-10 09:16:40 +0700

    oK

--- client/src/pages/ReportPages.jsx
+++ client/src/pages/ReportPages.jsx
@@ -1,10 +1,71 @@
 import React from "react";
-import { Flex, SimpleGrid, Text, Box, Icon } from "@chakra-ui/react";
+import {
+	Flex,
+	SimpleGrid,
+	Text,
+	Box,
+	Icon,
+	Stack,
+	HStack,
+} from "@chakra-ui/react";
+import {
+	AreaChart,
+	YAxis,
+	XAxis,
+	CartesianGrid,
+	Area,
+	Tooltip,
+} from "recharts";
 import SideBar from "../components/SideBar";
 import TopBarAdmin from "../components/TopBarAdmin";
 import { BsArrowUpRight } from "react-icons/bs";
 
 const ReportPages = () => {
+	const data = [
+		{
+			name: "Page A",
+			uv: 4000,
+			pv: 2400,
+			amt: 2400,
+		},
+		{
+			name: "Page B",
+			uv: 3000,
+			pv: 1398,
+			amt: 2210,
+		},
+		{
+			name: "Page C",
+			uv: 2000,
+			pv: 9800,
+			amt: 2290,
+		},
+		{
+			name: "Page D",
+			uv: 2780,
+			pv: 3908,
+			amt: 2000,
+		},
+		{
+			name: "Page E",
+			uv: 1890,
+			pv: 4800,
+			amt: 2181,
+		},
+		{
+			name: "Page F",
+			uv: 2390,
+			pv: 3800,
+			amt: 2500,
+		},
+		{
+			name: "Page G",
+			uv: 3490,
+			pv: 4300,
+			amt: 2100,
+		},
+	];
+
 	return (
 		<>
 			<Flex className="container">
@@ -174,26 +235,98 @@ const ReportPages = () => {
 								</Flex>
 							</Box>
 						</SimpleGrid>
-						<SimpleGrid columns={2} spacingX="10px" spacingY="20px" p={4}>
-							<Box
-								border={"1px solid black"}
-								borderRadius={"10px"}
-								height="50vh"
-								w={"35vw"}
-							>
-								<Flex>
-									<Text
-										fontSize={"10px"}
-										p={4}
-										color={"gray"}
-										fontFamily={"serif"}
-										fontWeight={"medium"}
+						<SimpleGrid columns={2} spacingX="20px" spacingY="20px" p={4}>
+							<Stack>
+								<HStack>
+									<Box
+										border={"1px solid black"}
+										borderRadius={"10px"}
+										height="50vh"
+										w={"39vw"}
 									>
-										Day of the Week Gross Sales Amount
-									</Text>
-								</Flex>
-							</Box>
-							<Box bg="tomato" height="80px"></Box>
+										<Flex>
+											<Text
+												fontSize={"10px"}
+												p={4}
+												color={"gray"}
+												fontFamily={"serif"}
+												fontWeight={"medium"}
+											>
+												Day of the Week Gross Sales Amount
+											</Text>
+										</Flex>
+										<AreaChart
+											width={460}
+											height={250}
+											data={data}
+											margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+										>
+											<defs>
+												<linearGradient
+													id="colorUv"
+													x1="0"
+													y1="0"
+													x2="0"
+													y2="1"
+												>
+													<stop
+														offset="5%"
+														stopColor="#8884d8"
+														stopOpacity={0.8}
+													/>
+													<stop
+														offset="95%"
+														stopColor="#8884d8"
+														stopOpacity={0}
+													/>
+												</linearGradient>
+												<linearGradient
+													id="colorPv"
+													x1="0"
+													y1="0"
+													x2="0"
+													y2="1"
+												>
+													<stop
+														offset="5%"
+														stopColor="#82ca9d"
+														stopOpacity={0.8}
+													/>
+													<stop
+														offset="95%"
+														stopColor="#82ca9d"
+														stopOpacity={0}
+													/>
+												</linearGradient>
+											</defs>
+											<XAxis dataKey="name" />
+											<YAxis />
+											<CartesianGrid strokeDasharray="3 3" />
+											<Tooltip />
+											<Area
+												type="monotone"
+												dataKey="uv"
+												stroke="#8884d8"
+												fillOpacity={1}
+												fill="url(#colorUv)"
+											/>
+											<Area
+												type="monotone"
+												dataKey="pv"
+												stroke="#82ca9d"
+												fillOpacity={1}
+												fill="url(#colorPv)"
+											/>
+										</AreaChart>
+									</Box>
+									<Box
+										border={"1px solid black"}
+										borderRadius={"10px"}
+										h="50vh"
+										w={"120px"}
+									></Box>
+								</HStack>
+							</Stack>
 						</SimpleGrid>
 					</Flex>
 				</Flex>
