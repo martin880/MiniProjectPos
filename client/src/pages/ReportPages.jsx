ca0d4979857e6ed6066a245f172f6f9fa4265a77
Martin Halomoan Lumbangaol <62680881+martin880@users.noreply.github.com>
2023-06-12 13:47:16 +0700

    Ok

--- client/src/pages/ReportPages.jsx
+++ client/src/pages/ReportPages.jsx
@@ -7,6 +7,7 @@ import {
   Icon,
   Stack,
   HStack,
+  VStack,
 } from "@chakra-ui/react";
 import {
   AreaChart,
@@ -15,6 +16,11 @@ import {
   CartesianGrid,
   Area,
   Tooltip,
+  PieChart,
+  Pie,
+  LineChart,
+  Line,
+  Legend
 } from "recharts";
 import SideBar from "../components/SideBar";
 import TopBarAdmin from "../components/TopBarAdmin";
@@ -125,6 +131,105 @@ const ReportPages = () => {
       amt: 2100,
     },
   ];
+
+  const data01 = [
+    {
+      "name": "Group A",
+      "value": 400
+    },
+    {
+      "name": "Group B",
+      "value": 300
+    },
+    {
+      "name": "Group C",
+      "value": 300
+    },
+    {
+      "name": "Group D",
+      "value": 200
+    },
+    {
+      "name": "Group E",
+      "value": 278
+    },
+    {
+      "name": "Group F",
+      "value": 189
+    }
+  ];
+  const data02 = [
+    {
+      "name": "Group A",
+      "value": 2400
+    },
+    {
+      "name": "Group B",
+      "value": 4567
+    },
+    {
+      "name": "Group C",
+      "value": 1398
+    },
+    {
+      "name": "Group D",
+      "value": 9800
+    },
+    {
+      "name": "Group E",
+      "value": 3908
+    },
+    {
+      "name": "Group F",
+      "value": 4800
+    }
+  ];
+
+  const samsung = [
+    {
+      "name": "Page A",
+      "uv": 4000,
+      "pv": 2400,
+      "amt": 2400
+    },
+    {
+      "name": "Page B",
+      "uv": 3000,
+      "pv": 1398,
+      "amt": 2210
+    },
+    {
+      "name": "Page C",
+      "uv": 2000,
+      "pv": 9800,
+      "amt": 2290
+    },
+    {
+      "name": "Page D",
+      "uv": 2780,
+      "pv": 3908,
+      "amt": 2000
+    },
+    {
+      "name": "Page E",
+      "uv": 1890,
+      "pv": 4800,
+      "amt": 2181
+    },
+    {
+      "name": "Page F",
+      "uv": 2390,
+      "pv": 3800,
+      "amt": 2500
+    },
+    {
+      "name": "Page G",
+      "uv": 3490,
+      "pv": 4300,
+      "amt": 2100
+    }
+  ]
+  
 
   const post = [
     {
@@ -295,11 +400,29 @@ const ReportPages = () => {
                   <Box
                     border={"1px solid black"}
                     borderRadius={"10px"}
-                    h="50vh"
-                    w={"120px"}
-                  ></Box>
+                    height="50vh"
+                    w={"39vw"}
+                  >
+                    <Flex w={'36vw'}>
+                    <PieChart width={330} height={300}>
+                      <Pie data={data02} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+                    </PieChart>
+                    </Flex>
+                  </Box>
                 </HStack>
               </Stack>
+            </SimpleGrid>
+            <SimpleGrid>
+            <LineChart width={960} height={250} data={samsung}
+              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
             </SimpleGrid>
           </Flex>
         </Flex>
