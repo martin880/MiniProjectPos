b5101f894bea75d4b3194551f47f83c2e6fbbdc4
ThomasAlvin <thomasalvinyeo@gmail.com>
2023-06-12 13:49:32 +0700

    small update

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -9,55 +9,66 @@ import {
 } from "@chakra-ui/react";
 import { useSelector, useDispatch } from "react-redux";
 import { addOrder, removeOrder } from "../redux/orderList";
+import { useEffect, useState } from "react";
+import { api } from "../api/api";
 
 export default function Product() {
-  const arr = [
-    {
-      status: "Available",
-      productName: "Espresso",
-      price: 22000,
-    },
-    {
-      status: "Available",
-      productName: "Cappuccino",
-      price: 28000,
-    },
-    {
-      status: "Available",
-      productName: "Coffee Latte",
-      price: 28000,
-    },
-    {
-      status: "Available",
-      productName: "Americano",
-      price: 24000,
-    },
-    {
-      status: "Available",
-      productName: "Mocha",
-      price: 30000,
-    },
-    {
-      status: "Available",
-      productName: "Macchiato",
-      price: 27000,
-    },
-    {
-      status: "Available",
-      productName: "Flat White",
-      price: 25000,
-    },
-    {
-      status: "Available",
-      productName: "Cold Brew",
-      price: 35000,
-    },
-  ];
+  const [products, setProducts] = useState([]);
+  async function fetchProducts() {
+    await api.get("/product").then((res) => {
+      setProducts(res.data);
+    });
+  }
+  useEffect(() => {
+    fetchProducts(); // Update nilai user jika prop berubah
+  }, []);
+  //   const arr = [
+  //     {
+  //       status: "Available",
+  //       productName: "Espresso",
+  //       price: 22000,
+  //     },
+  //     {
+  //       status: "Available",
+  //       productName: "Cappuccino",
+  //       price: 28000,
+  //     },
+  //     {
+  //       status: "Available",
+  //       productName: "Coffee Latte",
+  //       price: 28000,
+  //     },
+  //     {
+  //       status: "Available",
+  //       productName: "Americano",
+  //       price: 24000,
+  //     },
+  //     {
+  //       status: "Available",
+  //       productName: "Mocha",
+  //       price: 30000,
+  //     },
+  //     {
+  //       status: "Available",
+  //       productName: "Macchiato",
+  //       price: 27000,
+  //     },
+  //     {
+  //       status: "Available",
+  //       productName: "Flat White",
+  //       price: 25000,
+  //     },
+  //     {
+  //       status: "Available",
+  //       productName: "Cold Brew",
+  //       price: 35000,
+  //     },
+  //   ];
 
   return (
     <>
       <Grid templateColumns="repeat(4,2fr)" gap={3} w={"100%"}>
-        {arr.map((val) => (
+        {products.map((val) => (
           <Card
             key={val.productName}
             status={val.status}
@@ -71,25 +82,22 @@ export default function Product() {
 }
 
 function Card(props) {
+  const orderType = useSelector((state) => state.orderType.value);
+  const orderList = useSelector((state) => state.orderList);
+  const currQuantity = orderList.filter(
+    (item) =>
+      item.menu === `${props.productName}` && item.orderType === `${orderType}`
+  );
+  const dispatch = useDispatch();
+  const add = {
+    menu: `${props.productName}`,
+    unitPrice: `${props.price}`,
+    orderType: `${orderType}`,
+    specialRequest: [],
+    note: "",
+  };
 
-	const orderType = useSelector((state) => state.orderType.value);
-	const orderList = useSelector((state) => state.orderList);
-	const currQuantity = orderList.filter(
-		(item) =>
-			item.menu === `${props.productName}` &&
-			item.orderType === `${orderType}`
-	);
-	const dispatch = useDispatch();
-	const add = {
-		menu: `${props.productName}`,
-		unitPrice: `${props.price}`,
-		orderType: `${orderType}`,
-		specialRequest: [],
-		note: "",
-	};
-
-	// console.log("ORDER LIST STATE", orderList);
-
+  // console.log("ORDER LIST STATE", orderList);
 
   function PrintVal() {
     // console.log("CART", );
@@ -97,100 +105,89 @@ function Card(props) {
     // console.log("ORDER LIST STATE", orderList);
   }
 
-	return (
-		<GridItem
-			w={"100%"}
-			bg={"#CEDCD9"}
-			style={{ borderRadius: "15px" }}
-			maxH={"200px"}
-			// minH={"165px"}
-			minH={"45%"}
-			overflow={"hidden"}
-		>
-			<Flex flexDirection={"row"} h={"100%"} overflow={"hidden"}>
-				<Flex w={"10px"}></Flex>
-				<Flex
-					w={"calc(100% - 10px)"}
-					bg={"rgba(17, 19, 20, 0.85)"}
-					paddingLeft={"10px"}
-					paddingTop={"10px"}
-					flexDirection={"column"}
-					justifyContent={"space-between"}
-					overflow={"hidden"}
-				>
-					<Flex>
-						<Stat color={"#ACACAC"}>
-							<StatLabel fontSize={"12px"}>
-								{/* {props.status}  */}
-								{orderType}
-							</StatLabel>
-							<StatNumber
-								color={"whitesmoke"}
-								fontSize={"20px"}
-								paddingTop={"10px"}
-								onClick={PrintVal}
-							>
-								{props.productName}
-							</StatNumber>
-							<StatHelpText fontWeight={"500"}>
-								{props.price}
-							</StatHelpText>
-						</Stat>
-					</Flex>
-					<Flex
-						w={"100%"}
-						h={"45px"}
-						paddingRight={"10px"}
-						justifyContent={"end"}
-					>
-						<Flex
-							w={"80%"}
-							paddingBottom={"10px"}
-							overflow={"hidden"}
-						>
-							<Grid
-								templateColumns={"repeat(3,1fr)"}
-								gap={1}
-								w={"100%"}
-								fontSize={"19px"}
-								textAlign={"center"}
-								color={"white"}
-								overflow={"hidden"}
-							>
-								<GridItem
-									overflow={"hidden"}
-									className="qty-button"
-									color={
-										currQuantity.length ? "white" : "grey"
-									}
-									onClick={() => {
-										dispatch(
-											removeOrder({
-												productName: props.productName,
-												orderType: orderType,
-											})
-										);
-									}}
-								>
-									-
-								</GridItem>
-								<GridItem w={"100%"}>
-									{currQuantity.length}
-								</GridItem>
-								<GridItem
-									className="qty-button"
-									onClick={() => {
-										dispatch(addOrder(add));
-									}}
-								>
-									+
-								</GridItem>
-							</Grid>
-						</Flex>
-					</Flex>
-				</Flex>
-			</Flex>
-		</GridItem>
-	);
-
+  return (
+    <GridItem
+      w={"100%"}
+      bg={"#CEDCD9"}
+      style={{ borderRadius: "15px" }}
+      maxH={"200px"}
+      // minH={"165px"}
+      minH={"45%"}
+      overflow={"hidden"}
+    >
+      <Flex flexDirection={"row"} h={"100%"} overflow={"hidden"}>
+        <Flex w={"10px"}></Flex>
+        <Flex
+          w={"calc(100% - 10px)"}
+          bg={"rgba(17, 19, 20, 0.85)"}
+          paddingLeft={"10px"}
+          paddingTop={"10px"}
+          flexDirection={"column"}
+          justifyContent={"space-between"}
+          overflow={"hidden"}
+        >
+          <Flex>
+            <Stat color={"#ACACAC"}>
+              <StatLabel fontSize={"12px"}>
+                {/* {props.status}  */}
+                {orderType}
+              </StatLabel>
+              <StatNumber
+                color={"whitesmoke"}
+                fontSize={"20px"}
+                paddingTop={"10px"}
+                onClick={PrintVal}
+              >
+                {props.productName}
+              </StatNumber>
+              <StatHelpText fontWeight={"500"}>{props.price}</StatHelpText>
+            </Stat>
+          </Flex>
+          <Flex
+            w={"100%"}
+            h={"45px"}
+            paddingRight={"10px"}
+            justifyContent={"end"}
+          >
+            <Flex w={"80%"} paddingBottom={"10px"} overflow={"hidden"}>
+              <Grid
+                templateColumns={"repeat(3,1fr)"}
+                gap={1}
+                w={"100%"}
+                fontSize={"19px"}
+                textAlign={"center"}
+                color={"white"}
+                overflow={"hidden"}
+              >
+                <GridItem
+                  overflow={"hidden"}
+                  className="qty-button"
+                  color={currQuantity.length ? "white" : "grey"}
+                  onClick={() => {
+                    dispatch(
+                      removeOrder({
+                        productName: props.productName,
+                        orderType: orderType,
+                      })
+                    );
+                  }}
+                >
+                  -
+                </GridItem>
+                <GridItem w={"100%"}>{currQuantity.length}</GridItem>
+                <GridItem
+                  className="qty-button"
+                  onClick={() => {
+                    dispatch(addOrder(add));
+                  }}
+                >
+                  +
+                </GridItem>
+              </Grid>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Flex>
+    </GridItem>
+  );
 }
