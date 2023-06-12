0fa68583233b9ca6a9c2865464854046b3d4618f
ThomasAlvin <thomasalvinyeo@gmail.com>
2023-06-12 13:58:57 +0700

    update

--- client/src/pages/ReportPages.jsx
+++ client/src/pages/ReportPages.jsx
@@ -20,7 +20,7 @@ import {
   Pie,
   LineChart,
   Line,
-  Legend
+  Legend,
 } from "recharts";
 import SideBar from "../components/SideBar";
 import TopBarAdmin from "../components/TopBarAdmin";
@@ -134,129 +134,128 @@ const ReportPages = () => {
 
   const data01 = [
     {
-      "name": "Group A",
-      "value": 400
+      name: "Group A",
+      value: 400,
     },
     {
-      "name": "Group B",
-      "value": 300
+      name: "Group B",
+      value: 300,
     },
     {
-      "name": "Group C",
-      "value": 300
+      name: "Group C",
+      value: 300,
     },
     {
-      "name": "Group D",
-      "value": 200
+      name: "Group D",
+      value: 200,
     },
     {
-      "name": "Group E",
-      "value": 278
+      name: "Group E",
+      value: 278,
     },
     {
-      "name": "Group F",
-      "value": 189
-    }
+      name: "Group F",
+      value: 189,
+    },
   ];
   const data02 = [
     {
-      "name": "Group A",
-      "value": 2400
+      name: "Group A",
+      value: 2400,
     },
     {
-      "name": "Group B",
-      "value": 4567
+      name: "Group B",
+      value: 4567,
     },
     {
-      "name": "Group C",
-      "value": 1398
+      name: "Group C",
+      value: 1398,
     },
     {
-      "name": "Group D",
-      "value": 9800
+      name: "Group D",
+      value: 9800,
     },
     {
-      "name": "Group E",
-      "value": 3908
+      name: "Group E",
+      value: 3908,
     },
     {
-      "name": "Group F",
-      "value": 4800
-    }
+      name: "Group F",
+      value: 4800,
+    },
   ];
 
   const samsung = [
     {
-      "name": "Page A",
-      "uv": 4000,
-      "pv": 2400,
-      "amt": 2400
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
     },
     {
-      "name": "Page B",
-      "uv": 3000,
-      "pv": 1398,
-      "amt": 2210
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
     },
     {
-      "name": "Page C",
-      "uv": 2000,
-      "pv": 9800,
-      "amt": 2290
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
     },
     {
-      "name": "Page D",
-      "uv": 2780,
-      "pv": 3908,
-      "amt": 2000
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
     },
     {
-      "name": "Page E",
-      "uv": 1890,
-      "pv": 4800,
-      "amt": 2181
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
     },
     {
-      "name": "Page F",
-      "uv": 2390,
-      "pv": 3800,
-      "amt": 2500
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
     },
     {
-      "name": "Page G",
-      "uv": 3490,
-      "pv": 4300,
-      "amt": 2100
-    }
-  ]
-  
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
 
   const post = [
     {
-      "id": 1,
-      "title":"Gross Sale",
-      "price":"8.999.999",
-      "gross":"9.10"
+      id: 1,
+      title: "Gross Sale",
+      price: "8.999.999",
+      gross: "9.10",
     },
     {
-      "id": 2,
-      "title":"Gross Sale",
-      "price":"8.999.999",
-      "gross":"9.10"
+      id: 2,
+      title: "Gross Sale",
+      price: "8.999.999",
+      gross: "9.10",
     },
     {
-      "id": 3,
-      "title":"Gross Sale",
-      "price":"8.999.999",
-      "gross":"9.10"
+      id: 3,
+      title: "Gross Sale",
+      price: "8.999.999",
+      gross: "9.10",
     },
     {
-      "id": 4,
-      "title":"Gross Sale",
-      "price":"8.999.999",
-      "gross":"9.10"
-    }
-  ]
+      id: 4,
+      title: "Gross Sale",
+      price: "8.999.999",
+      gross: "9.10",
+    },
+  ];
 
   return (
     <>
@@ -270,47 +269,47 @@ const ReportPages = () => {
               <TopBarAdmin />
             </Flex>
             <SimpleGrid minChildWidth="120px" spacing="30px" p={4}>
-              {post.map(post => (
+              {post.map((post) => (
                 <Box
-                key={post.id}
-                border={"1px solid black"}
-                borderRadius={"10px"}
-                height="80px"
-              >
-                <Flex>
-                  <Text
-                    fontSize={"10px"}
-                    p={3}
-                    color={"gray"}
-                    fontFamily={"serif"}
-                    fontWeight={"medium"}
-                  >
-                    {post.title}
-                  </Text>
-                </Flex>
-                <Flex>
-                  <Text
-                    fontSize={"14px"}
-                    p={3}
-                    color={"black"}
-                    fontFamily={"sans-serif"}
-                    fontWeight={"bold"}
-                  >
-                    {post.price}
-                  </Text>
-                  <Flex flexDir={"column"}>
+                  key={post.id}
+                  border={"1px solid black"}
+                  borderRadius={"10px"}
+                  height="80px"
+                >
+                  <Flex>
                     <Text
-                      fontSize={"8px"}
-                      p={4}
-                      color={"green"}
+                      fontSize={"10px"}
+                      p={3}
+                      color={"gray"}
+                      fontFamily={"serif"}
+                      fontWeight={"medium"}
+                    >
+                      {post.title}
+                    </Text>
+                  </Flex>
+                  <Flex>
+                    <Text
+                      fontSize={"14px"}
+                      p={3}
+                      color={"black"}
                       fontFamily={"sans-serif"}
                       fontWeight={"bold"}
                     >
-                      {post.gross}
+                      {post.price}
                     </Text>
+                    <Flex flexDir={"column"}>
+                      <Text
+                        fontSize={"8px"}
+                        p={4}
+                        color={"green"}
+                        fontFamily={"sans-serif"}
+                        fontWeight={"bold"}
+                      >
+                        {post.gross}
+                      </Text>
+                    </Flex>
                   </Flex>
-                </Flex>
-              </Box>
+                </Box>
               ))}
             </SimpleGrid>
             <SimpleGrid columns={2} spacingX="20px" spacingY="20px" p={4}>
@@ -403,26 +402,40 @@ const ReportPages = () => {
                     height="50vh"
                     w={"39vw"}
                   >
-                    <Flex w={'36vw'}>
-                    <PieChart width={330} height={300}>
-                      <Pie data={data02} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
-                    </PieChart>
+                    <Flex w={"36vw"}>
+                      <PieChart width={330} height={300}>
+                        <Pie
+                          data={data02}
+                          dataKey="value"
+                          nameKey="name"
+                          cx="50%"
+                          cy="50%"
+                          innerRadius={60}
+                          outerRadius={80}
+                          fill="#82ca9d"
+                          label
+                        />
+                      </PieChart>
                     </Flex>
                   </Box>
                 </HStack>
               </Stack>
             </SimpleGrid>
             <SimpleGrid>
-            <LineChart width={960} height={250} data={samsung}
-              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
+              <LineChart
+                width={960}
+                height={250}
+                data={samsung}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+              </LineChart>
             </SimpleGrid>
           </Flex>
         </Flex>
